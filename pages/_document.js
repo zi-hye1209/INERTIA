1b83d7a72affcec5337124c24b62956274dcfef7
Jihye <gjjh1111@kaist.ac.kr>
2022-04-30 11:40:35 +0900

    bla

--- pages/_document.js
+++ pages/_document.js
@@ -17,14 +17,14 @@ export default class MyDocument extends Document {
                             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-KWTZ4FK');`,
+                            })(window,document,'script','dataLayer','GTM-TWT4Q77');`,
                         }}
                     />
                 </Head>
                 <body>
                     <noscript>
                         <iframe
-                            src="https://www.googletagmanager.com/ns.html?id=GTM-KWTZ4FK"
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-TWT4Q77"
                             height="0"
                             width="0"
                             hidden
